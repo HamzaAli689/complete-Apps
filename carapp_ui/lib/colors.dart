14d78981a37e6753ab52e2bb9e74fbdf7395079e
Hamza Ali <hamzaalivr33@gmail.com>
2025-01-10 15:03:58 +0500

    start making Car app UI

---
+++ carapp_ui/lib/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color whiteSmoke = Color(0xFFF5F5F5); // Almost White
+  static const Color gray = Color(0xFF82888F);
+  static const Color gunmetal = Color(0xFF242A2F); // Deep Gray-Blue & Gray-Blue
+  static const Color aquamarine = Color(0xFF1EFFAA);
+  static const Color onyx = Color(0xFF3E4044); // Dark Blue-Gray & Medium Blue-Gray
+}
