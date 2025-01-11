d8ab513a1be00a32f66cd0256069ecf32620fbcb
Hamza Ali <hamzaalivr33@gmail.com>
2025-01-11 20:04:46 +0500

    update the code

---
+++ carapp_ui/lib/Screens/HomeScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../constant/colors.dart';
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: onyx,
+      body: Text("Hamza Ali"),
+    );
+  }
+}
