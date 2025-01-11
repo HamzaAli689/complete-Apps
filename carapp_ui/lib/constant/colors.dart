97ed986e6c0d2c45255fb718cf16023b1dbd5f3a
Hamza Ali <hamzaalivr33@gmail.com>
2025-01-11 20:15:22 +0500

    change the name of colors

--- carapp_ui/lib/constant/colors.dart
+++ carapp_ui/lib/constant/colors.dart
@@ -2,6 +2,6 @@ import 'dart:ui';
 
 const Color whiteSmoke = Color(0xFFF5F5F5); // Almost White
 const Color gray = Color(0xFF82888F);
-const Color gunmetal = Color(0xFF242A2F); // Deep Gray-Blue & Gray-Blue
-const Color aquamarine = Color(0xFF1EFFAA);
+const Color backcolor = Color(0xFF242A2F); // Deep Gray-Blue & Gray-Blue
+const Color green = Color(0xFF1EFFAA);
 const Color onyx = Color(0xFF3E4044); // Dark Blue-Gray & Medium Blue-Gray
