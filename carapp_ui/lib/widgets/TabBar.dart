01f3b3f29907147696975db73ec838ed71f10811
Hamza Ali <hamzaalivr33@gmail.com>
2025-01-12 13:55:28 +0500

    update the code

--- carapp_ui/lib/widgets/TabBar.dart
+++ carapp_ui/lib/widgets/TabBar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 import '../constant/colors.dart';
 
@@ -15,11 +16,14 @@ class Tabbar extends StatelessWidget {
             TabBar(
               indicator: BoxDecoration(
                 borderRadius: BorderRadius.circular(25.0),
-                color: whiteSmoke,
+                color: green,
               ),
-              labelColor: Colors.black, // Text color for the selected tab
-              unselectedLabelColor: Colors.white, // For unselected tabs
-              indicatorSize: TabBarIndicatorSize.tab, // Fill the tab
+              labelColor: Colors.black,
+              // Text color for the selected tab
+              unselectedLabelColor: Colors.white,
+              // For unselected tabs
+              indicatorSize: TabBarIndicatorSize.tab,
+              // Fill the tab
               dividerColor: Colors.transparent,
               tabs: [
                 Tab(text: 'Same Drop-Off'),
@@ -29,15 +33,88 @@ class Tabbar extends StatelessWidget {
             Expanded(
               child: TabBarView(
                 children: [
-                  Center(
-                    child: Text(
-                      'Content for Same Drop-Off',
-                      style: TextStyle(fontSize: 16, color: whiteSmoke),
+                  SingleChildScrollView(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                        color: whiteSmoke,
+                      ),
+                      child: Column(
+                        children: [
+                          Gap(20),
+                          ListTile(
+                            leading: Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                border: Border.all(color: gray),
+                              ),
+                              child: Icon(
+                                Icons.location_on_outlined,
+                                size: 30,
+                              ),
+                            ),
+                            title: Text(
+                              "Pick-Up",
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.normal,
+                                  color: gray),
+                            ),
+                            subtitle: Text(
+                              "Dubai Airport",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                    child: Divider(
+                                  color: gray,
+                                  height: 1,
+                                )),
+                                Gap(3),
+                                Container(
+                                  height: 40,
+                                  width: 40,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    border: Border.all(color: gray),
+                                  ),
+                                  child:  Image.asset("assets/arrow.png"),
+                                ),
+                              ],
+                            ), 
+                          ),
+                          Gap(20),
+                          ListTile(
+                            leading: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                border: Border.all(color: gray),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   Center(
                     child: Text(
-                      'Content for Different Drop-Off',
+                      'Content for Same Drop-Off',
                       style: TextStyle(fontSize: 16, color: whiteSmoke),
                     ),
                   ),
