4887d23e37cfa991e5503914a44511d151ccc7db
Hamza Ali <hamzaalivr33@gmail.com>
2025-01-14 16:39:24 +0500

    create the  app bar

--- carapp_ui/lib/Screens/SelectTheCar.dart
+++ carapp_ui/lib/Screens/SelectTheCar.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
 
+import '../constant/colors.dart';
+
 class SelectTheCar extends StatelessWidget {
   const SelectTheCar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: backcolor,
+      appBar: AppBar(
+        backgroundColor: backcolor,
+        leading: Container(
+          height: 40,
+          width: 40,
+          decoration: BoxDecoration(
+            color: backcolor,
+            borderRadius: BorderRadius.circular(50),
+            border: Border.all(color: gray),
+          ),
+          child: Icon(
+            Icons.location_on_outlined,
+            color: Colors.white,
+            size: 30,
+          ),
+        ),
+        title: Column(
+          children: [
+            Text(
+              "Dubai Airport",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                letterSpacing: 1,
+              ),
+            ),
+            Text(
+              "Jan 13 2025 - Jan 18 2025",
+              style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.normal,
+                  color: gray),
+            ),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+      ),
+    );
   }
 }
