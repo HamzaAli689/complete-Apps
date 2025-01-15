cef8c860b14ec981cca356036e5872b39b7c8f16
Hamza Ali <hamzaalivr33@gmail.com>
2025-01-15 20:46:02 +0500

    create the carContainer

---
+++ carapp_ui/lib/widgets/CarContainer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Carcontainer extends StatelessWidget {
+  const Carcontainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Placeholder();
+  }
+}
