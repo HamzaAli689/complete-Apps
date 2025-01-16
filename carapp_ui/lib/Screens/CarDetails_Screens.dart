5a60df7c12acc2b525ec1967da3ca42c6e424799
Hamza Ali <hamzaalivr33@gmail.com>
2025-01-16 23:23:23 +0500

    add the stl class

---
+++ carapp_ui/lib/Screens/CarDetails_Screens.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CardetailsScreens extends StatelessWidget {
+  const CardetailsScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
